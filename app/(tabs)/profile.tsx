630a9ad7e36fe7dc8f20e97c7d4b3ff1ae7e8839
moknine elhadj <mokninemoknine999@gmail.com>
2025-04-24 21:26:30 +0100

    start building user profile screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,47 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { styles } from "@/styles/feed.styles";
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { useAuth } from "@clerk/clerk-expo";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { Doc } from "@/convex/_generated/dataModel";
+import Loader from "@/components/Loader";
+import COLORS from "@/constant/theme";
+import { Ionicons } from "@expo/vector-icons";
+import { styles } from "@/styles/profile.styles";
 
 export default function profile() {
+  const { signOut, userId } = useAuth();
+  const currentUser = useQuery(
+    api.users.getUserByClerkId,
+    userId ? { clerkId: userId } : "skip"
+  );
+  const [isEditModalVisible, setIsEditModalVisible] = useState(false);
+  const [editedProfile, setEditedProfile] = useState({
+    fullName: currentUser?.fullName || "",
+    bio: currentUser?.bio || "",
+  });
+
+  const [selectedPost, setSelectedPost] = useState<Doc<"posts"> | null>(null);
+  const posts = useQuery(api.posts.getPostsByUserId, {});
+  const updateProfile = useMutation(api.users.updateProfile);
+
+  const handleSaveProfile = async () => {};
+
+  if (!currentUser || posts === undefined) return <Loader />;
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}></View>
-      <Text>notifications</Text>
+      {/* Header */}
+      <View style={styles.header}>
+        <View style={styles.headerLeft}>
+          <Text style={styles.username}>{currentUser?.username}</Text>
+        </View>
+        <View style={styles.headerRight}>
+          <TouchableOpacity style={styles.headerIcon} onPress={() => signOut()}>
+            <Ionicons name="log-out-outline" size={24} color={COLORS.white} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
--- convex/users.ts
+++ convex/users.ts
@@ -57,3 +57,18 @@ export async function getAuthenticatedUser(ctx: QueryCtx | MutationCtx) {
   }
   return currentUser;
 }
+
+export const updateProfile = mutation({
+  args: {
+    fullName: v.string(),
+    bio: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const currentUser = await getAuthenticatedUser(ctx);
+
+    await ctx.db.patch(currentUser._id, {
+      fullName: args.fullName,
+      bio: args.bio,
+    });
+  },
+});
